d2d00a1fe130b4fae0d48dbac59922383bcefc4b
Vijay Gaikwad <vijaygkd@gmail.com>
2025-07-04 16:57:17 -0700

    notebook with Simple grpo implementation

---
+++ rft.py
@@ -0,0 +1,17 @@
+### RFT - Reinforcement Fine-Tuning main file
+
+
+# 1. Load model - Llama7b model
+
+# 2. Load dataset - Wordle
+
+# 3. Build reward function
+
+# 4. GRPO implementation - loss
+
+# 5. Training loop
+
+# 6. Bechmarking & Evaluation
+
+
+
